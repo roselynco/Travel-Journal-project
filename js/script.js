c55028c29df3f9bf36e4bf4345c6a522ca443205
roselynco <181357945+roselynco@users.noreply.github.com>
2025-03-24 17:16:36 -0400

    Added the navigation bar and page routing.
    git remote add origin https://github.com/roselynco/Travel-Journal-project---Week-1-5.git
    git push
    
    '

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll("nav ul li a");
+    const sections = document.querySelectorAll(".page");
+
+    function showPage(pageId) {
+        sections.forEach((section) => {
+            section.classList.remove("active");
+        
+    });
+    document.getElementId(pageId).classList.add("active");
+
+    }
+
+    link.forEach ((link) => {
+        link.addEventListener("click", function(event) {
+            event.ptrventdefault ();
+            const pageId = this.getAttribute("date-page");
+            showPage(pageId);
+    });
+  });
+
+  showPage("home");
+});
